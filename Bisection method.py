63997c352b274f0479181f43d4e6f059219f0bbd
Erik Peteri <peteri.erik@gmail.com>
2023-07-28 17:42:52 +0300

    refactoring code

--- Bisection method.py
+++ Bisection method.py
@@ -1,55 +1,80 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import sympy as sp
 
-counter = 0
-function = input("Function: ")
-quantity = int(input("Quantity: "))
-initial_value = int(input("Initial value: "))
-final_value = int(input("Final value: "))
-precision = int(input("Precision: "))
+def input_integer(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
 
+def input_function():
+    return input("Function: ")
 
-def f(x):
-    evaluation = eval(function)
-    return evaluation
+def validate_interval(a, b, func, num_segments=100):
+    x_values = np.linspace(a, b, num=num_segments)
+    y_values = [func(x) for x in x_values]
 
-if f(initial_value) > 0 and f(final_value) < 0 or f(initial_value) < 0 and f(final_value) > 0:
-    interval = f"]{initial_value}, {final_value}["
-    print(f"Function has a solution in interval {interval}.")
+    for i in range(len(y_values) - 1):
+        if y_values[i] * y_values[i + 1] <= 0:
+            return True
 
-    x = np.linspace(-6, 6, num=1000)
-    y = f(x)
+    return False
 
-    fig, ax = plt.subplots()
-    plt.grid()
-    ax.plot(x,y, color="blue")
-    ax.set_aspect('equal')
-    ax.grid(True, which='both')
-    ax.axhline(y=0, color='k')
-    ax.axvline(x=0, color='k')
-    ax.set_xlim(-3, 5)
-    ax.set_ylim(-3, 5)
+def bisection_method(func, a, b, tolerance):
+    roots = []
+    while a < b:
+        if func(a) == 0:
+            roots.append(a)
+            break
+        if func(b) == 0:
+            roots.append(b)
+            break
 
-    plt.plot(x, y, label="f(x)")
-    plt.legend()
-    plt.xlabel("x - axis")
-    plt.ylabel("y - axis")
-    plt.title("Graph")
-    plt.show()
-
-    while counter <= quantity:
-        midway = 0.5 * (initial_value + final_value)
-        value_a = f(initial_value)
-        value_c = f(midway)
+        c = (a + b) / 2
+        value_a = func(a)
+        value_c = func(c)
         product_ac = value_a * value_c
-        counter += 1
-        if product_ac >= 0:
-            initial_value = midway
-        elif product_ac <= 0:
-            final_value = midway
-    print("c =", round(midway, precision))
+        if np.abs(value_c) < tolerance:
+            roots.append(c)
+            a = c
+        elif product_ac < 0:
+            b = c
+        else:
+            a = c
 
-else:
-    interval = f"]{initial_value}, {final_value}["
-    print(f"Function has no solution in interval {interval}.")
+    return roots
 
+if __name__ == "__main__":
+    function = input_function()
+    quantity = input_integer("Quantity: ")
+    initial_value = input_integer("Initial value: ")
+    final_value = input_integer("Final value: ")
+    precision = input_integer("Precision: ")
+
+    x = np.linspace(initial_value, final_value, num=1000)
+    f = sp.lambdify(sp.Symbol('x'), sp.sympify(function))
+
+    if validate_interval(initial_value, final_value, f):
+        interval = f"]{initial_value}, {final_value}["
+        print(f"Function has a solution in interval {interval}.")
+
+        y = f(x)
+
+        plt.plot(x, y, label="f(x)")
+        plt.axhline(y=0, color='k')
+        plt.axvline(x=0, color='k')
+        plt.grid(True, which='both')
+        plt.legend()
+        plt.xlabel("x - axis")
+        plt.ylabel("y - axis")
+        plt.title("Graph")
+        plt.show()
+
+        roots = bisection_method(f, initial_value, final_value, 1e-6)
+        print("The root is:", ", ".join(f"c = {round(root, precision)}" for root in roots))
+
+    else:
+        interval = f"]{initial_value}, {final_value}["
+        print(f"Function has no solution in interval {interval}.")
